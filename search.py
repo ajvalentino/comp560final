bd802dcbbee2f8b20e6152340d729238d33a3dc0
graceku72 <graceku72@gmail.com>
2025-04-17 10:26:24 -0400

    Updates for slightly faster computation

--- search.py
+++ search.py
@@ -1,13 +1,12 @@
 import heapq
-from copy import deepcopy
 from data_creation import merged_df
 
 # Parameters (just examples for now)
-number_of_meals = 5
-calorie_goal = 300
-required_ingredients = set(['sausage'])
+number_of_meals = 4
+calorie_goal = 310
+required_ingredients = set(['sausage', 'hoagie roll'])
 # available_recipes = merged_df
-available_recipes = merged_df.head(50)
+available_recipes = merged_df.head(200)
 
 # Weight tuning - adjust according to goals (doesn't have to be between 0 and 1)
 w1, w2, w3 = 1, 1, 1
@@ -18,7 +17,7 @@ w1, w2, w3 = 1, 1, 1
 class State:
     def __init__(self, meal_plan, ingredients_used, total_calories):
         self.meal_plan = meal_plan
-        self.ingredients_used = ingredients_used
+        self.ingredients_used = set(ingredients_used)  # using sets for faster computation
         self.total_calories = total_calories
 
     def calories_per_meal(self):
@@ -35,14 +34,17 @@ def is_goal(state, number_of_meals, calorie_goal, required_ingredients):
 
 # Heuristic (h)
 def heuristic(state):
-    used = set(state.ingredients_used)
+    used = state.ingredients_used
     required_remaining = len(required_ingredients - used)
-
     calorie_diff = abs(state.calories_per_meal() - calorie_goal)
 
     # Estimate additional ingredients needed in next recipe
     if len(state.meal_plan) < number_of_meals:
-        avg_new_ings = available_recipes['ingredient_names'].apply(lambda ings: len(set(ings) - used)).mean()
+        # Faster: loop through ings instead of apply method with lambda
+        total_new_ings = 0
+        for ings in available_recipes['ingredient_names']:
+            total_new_ings += len(set(ings) - used)
+        avg_new_ings = total_new_ings / len(available_recipes)
     else:
         avg_new_ings = 0
 
@@ -51,23 +53,23 @@ def heuristic(state):
 # Cost so far (g)
 def path_cost(state):
     calorie_diff = abs(state.calories_per_meal() - calorie_goal)
-    return w1 * len(set(state.ingredients_used)) + w2 * calorie_diff
+    return w1 * len(state.ingredients_used) + w2 * calorie_diff
 
 # Successor generation
 def get_successors(state):
     successors = []
     used_ids = {r['id'] for r in state.meal_plan}
-    for _, row in available_recipes.iterrows():
-        if row['id'] in used_ids:
+    for row in available_recipes.itertuples(index=False):
+        if row.id in used_ids:
             continue
         new_meal_plan = state.meal_plan + [{
-            'id': row['id'],
-            'name': row['name'],
-            'calorie_count': row['calorie_count'],
-            'ingredients': row['ingredient_names']
+            'id': row.id,
+            'name': row.name,
+            'calorie_count': row.calorie_count,
+            'ingredients': row.ingredient_names
         }]
-        new_ingredients = deepcopy(state.ingredients_used) + row['ingredient_names']
-        new_calories = state.total_calories + row['calorie_count']
+        new_ingredients = state.ingredients_used | set(row.ingredient_names)
+        new_calories = state.total_calories + row.calorie_count
         successors.append(State(new_meal_plan, new_ingredients, new_calories))
     return successors
 
@@ -82,7 +84,7 @@ def astar_iterative_deepening():
         depth_limit += 1
 
 def astar_limited(max_depth):
-    initial_state = State([], [], 0)
+    initial_state = State([], set(), 0)
     frontier = [(path_cost(initial_state) + heuristic(initial_state), initial_state)]
     visited = set()
 
@@ -102,4 +104,4 @@ def astar_limited(max_depth):
 
 meal_plan = astar_iterative_deepening()
 for recipe in meal_plan:
-    print(f"ID: {recipe['id']}, Recipe: {recipe['name']}, Calories: {recipe['calorie_count']}") # get recipe links?
+    print(f"ID: {recipe['id']}, Recipe Name: {recipe['name']}, Calories: {recipe['calorie_count']}") # get recipe links?
